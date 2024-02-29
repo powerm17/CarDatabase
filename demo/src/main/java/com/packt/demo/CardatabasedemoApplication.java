b0e0e361e27113f8a64247f017e0e5ae5c816747
powerm17 <105803016+powerm17@users.noreply.github.com>
2024-02-29 16:48:03 -0500

    Test Logging messages
    Spring Boot Logging

--- demo/src/main/java/com/packt/demo/CardatabasedemoApplication.java
+++ demo/src/main/java/com/packt/demo/CardatabasedemoApplication.java
@@ -2,12 +2,17 @@ package com.packt.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardatabasedemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CardatabasedemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabasedemoApplication.class, args);
+		logger.info("Hello Spring Boot");
 	}
-
 }
