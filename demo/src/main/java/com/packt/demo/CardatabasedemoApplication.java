4a520715fb7833f077d327196e2dc57a0e5e0c4a
powerm17 <105803016+powerm17@users.noreply.github.com>
2024-02-29 16:39:57 -0500

    Import Spring boot Maven CarddatabaseApplication

---
+++ demo/src/main/java/com/packt/demo/CardatabasedemoApplication.java
@@ -0,0 +1,13 @@
+package com.packt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardatabasedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabasedemoApplication.class, args);
+	}
+
+}
